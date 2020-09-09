9d210d341b8fe848b9fb6bfc86c64eed68339ea6
Kamal Subedi <51923658+kamalsubedicoder@users.noreply.github.com>
2020-09-09 17:04:57 +0530

    Class

--- main.cpp
+++ main.cpp
@@ -2,13 +2,35 @@
 
 using namespace std;
 
+class number
+{
+    private:
+        int num;
+    public:
+        void input()
+        {
+
+            cin>>num;
+        }
+        void check()
+        {
+            if (num%2==0)
+            {
+                cout<<"The given number is even number"<<endl;
+            }
+            else
+            {
+                cout<<"The given number is odd number"<<endl;
+            }
+
+
+        }
+
+};
 int main()
 {
-    int p=100;
-    int *q;
-    q=&p;
-    cout<<p<< endl;
-    cout<<q<< endl;
-    cout<<*q<< endl;
+    number n1;
+    n1.input();
+    n1.check();
     return 0;
 }
